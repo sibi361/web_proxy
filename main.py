d48647dd5ad7a625ba98507af2d2187eafaefaf2
sibi361 <104310887+sibi361@users.noreply.github.com>
2024-05-16 00:36:01 +0530

    Add Proxy To hrefs

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI, Request, Response
+from fastapi.exceptions import HTTPException
 from starlette.responses import JSONResponse
 import requests
 from urllib.parse import urlsplit, urlunsplit
@@ -27,10 +28,16 @@ def clean_headers(headers):
     return proxy_headers
 
 
-def get_root_url(url):
+def get_url_root(url):
     split_url = urlsplit(url)
 
     return f"{split_url.scheme}://{split_url.netloc}"
+
+
+def get_url_query(url):
+    split_url = urlsplit(url)
+
+    return f"{split_url.path}{'?' if split_url.query else ''}{split_url.query}"
 
 
 # You now have:
@@ -42,7 +49,7 @@ def get_root_url(url):
 
 
 def replace_urls(html, url):
-    root_url = get_root_url(url)
+    root_url = get_url_root(url)
 
     fixedHtml = html.replace('src="/', f'src="{PROXY_PREFIX}{root_url}/')
     fixedHtml = fixedHtml.replace("src='/", f"src='{PROXY_PREFIX}{root_url}/")
@@ -50,16 +57,79 @@ def replace_urls(html, url):
     fixedHtml = fixedHtml.replace('href="/', f'href="{PROXY_PREFIX}{root_url}/')
     fixedHtml = fixedHtml.replace("href='/", f"href='{PROXY_PREFIX}{root_url}/")
 
-    fixedHtml = html.replace('src="https://', f'src="{PROXY_PREFIX}https://')
-    fixedHtml = fixedHtml.replace("src='https://", f"src='{PROXY_PREFIX}https://")
+    ###
 
-    fixedHtml = fixedHtml.replace('href="http://', f'href="{PROXY_PREFIX}http://')
-    fixedHtml = fixedHtml.replace("href='http://", f"href='{PROXY_PREFIX}http://")
+    #
+    # fixedHtml = fixedHtml.replace('src="http://', f'src="{PROXY_PREFIX}http://')
+    # fixedHtml = fixedHtml.replace("src='http://", f"src='{PROXY_PREFIX}http://")
 
-    fixedHtml = fixedHtml.replace('href="https://', f'href="{PROXY_PREFIX}https://')
-    fixedHtml = fixedHtml.replace("href='https://", f"href='{PROXY_PREFIX}https://")
+    # fixedHtml = fixedHtml.replace('href="http://', f'href="{PROXY_PREFIX}http://')
+    # fixedHtml = fixedHtml.replace("href='http://", f"href='{PROXY_PREFIX}http://")
+
+    ###
+
+    # fixedHtml = fixedHtml.replace('src="https://', f'src="{PROXY_PREFIX}https://')
+    # fixedHtml = fixedHtml.replace("src='https://", f"src='{PROXY_PREFIX}https://")
+
+    # fixedHtml = fixedHtml.replace('href="https://', f'href="{PROXY_PREFIX}https://')
+    # fixedHtml = fixedHtml.replace("href='https://", f"href='{PROXY_PREFIX}https://")
+
+    ###
+
+    # fixedHtml = fixedHtml.replace('src="http://', f'src="{PROXY_PREFIX}http://')
+    # fixedHtml = fixedHtml.replace("src='http://", f"src='{PROXY_PREFIX}http://")
+
+    # fixedHtml = fixedHtml.replace('src="https://', f'src="{PROXY_PREFIX}https://')
+    # fixedHtml = fixedHtml.replace("src='https://", f"src='{PROXY_PREFIX}https://")
+
+    # fixedHtml = fixedHtml.replace('href="http://', f'href="{PROXY_PREFIX}http://')
+    # fixedHtml = fixedHtml.replace("href='http://", f"href='{PROXY_PREFIX}http://")
+
+    # fixedHtml = fixedHtml.replace('href="https://', f'href="{PROXY_PREFIX}https://')
+    # fixedHtml = fixedHtml.replace("href='https://", f"href='{PROXY_PREFIX}https://")
 
     return fixedHtml
+
+
+def proxy(req, url):
+    proxy_req_headers = clean_headers(req.headers)
+
+    # print(proxy_resp_headers)
+    resp = requests.get(url, headers=proxy_req_headers)
+    # resp = requests.get(url, headers=proxy_req_headers, verify=False)
+
+    proxy_resp_headers = clean_headers(resp.headers)
+
+    # print(proxy_resp_headers)
+
+    try:
+        proxy_resp_content = (
+            replace_urls(resp.text, url)
+            if resp.headers["Content-type"].startswith("text/html")
+            else resp.text
+        )
+    except KeyError:
+        try:
+            proxy_resp_content = (
+                replace_urls(resp.text, url)
+                if resp.headers["content-type"].startswith("text/html")
+                else resp.text
+            )
+        except KeyError:
+            proxy_resp_content = resp.text
+
+    return Response(proxy_resp_content, headers=proxy_resp_headers)
+
+
+def handle_404(req: Request, exc: HTTPException):
+    try:
+        url = req.headers["Referer"]
+    except KeyError:
+        return JSONResponse({"success": False, "msg": "Not found"}, 404)
+
+    root_url = get_url_root(url)
+
+    return proxy(req, f"{root_url}{get_url_query(str(req.url))}")
 
 
 @app.get("/")
@@ -73,7 +143,7 @@ async def healthcheck():
 
 
 @app.get("/proxy")
-async def proxy(req: Request):
+async def proxy_handler(req: Request):
     try:
         url = req.query_params["url"]
     except KeyError:
@@ -81,31 +151,11 @@ async def proxy(req: Request):
 
     # try:
     if True:
-        proxy_req_headers = clean_headers(req.headers)
-
-        # print(proxy_resp_headers)
-        resp = requests.get(url, headers=proxy_req_headers, verify=False)
-
-        proxy_resp_headers = clean_headers(resp.headers)
-
-        # print(proxy_resp_headers)
-
-        try:
-            proxy_resp_content = (
-                replace_urls(resp.text, url)
-                if resp.headers["Content-type"].startswith("text/html")
-                else resp.text
-            )
-        except KeyError:
-            try:
-                proxy_resp_content = (
-                    replace_urls(resp.text, url)
-                    if resp.headers["content-type"].startswith("text/html")
-                    else resp.text
-                )
-            except KeyError:
-                proxy_resp_content = resp.text
-
-        return Response(proxy_resp_content, headers=proxy_resp_headers)
+        return proxy(req, url)
     # except Exception as e:
     #     return JSONResponse({"success": False, "msg": str(e)}, 400)
+
+
+@app.exception_handler(404)
+def not_found_exception_handler(req: Request, exc: HTTPException):
+    return handle_404(req, exc)
